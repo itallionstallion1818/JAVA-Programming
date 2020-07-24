4a0c6ec8fef75077a7e4b3e488fd37d96b0c63ac
Aditya Prasad <ap379436@gmail.com>
2020-07-24 10:34:16 +0530

    Product of two numbers

---
+++ product.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class product{
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter the first element:");
+		a=s.nextInt();
+		System.out.println();
+		System.out.print("Enter the second element:");
+		b=s.nextInt();
+		System.out.println();
+
+		c=a*b;
+		System.out.print("The product of the numbers is="+c);
+	}
+}
